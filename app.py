d05967e4afdd02b16c83f92174da3fd5aea14af4
Suraj3571 <surajwaragade2@gmail.com>
2024-07-01 21:30:45 +0530

    Home page

--- app.py
+++ app.py
@@ -11,11 +11,8 @@ app=application
 
 ## Route for a home page
 
-@app.route('/')
-def index():
-    return render_template('index.html') 
 
-@app.route('/predictdata',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
@@ -42,5 +39,5 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")        
+    app.run(host="0.0.0.0", debug = True)        
 
